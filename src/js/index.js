3edb429ec916a061fefca4031211e74bab3b5aef
Kyderom <eenzo.nicoletti@gmail.com>
2025-01-27 00:04:13 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+console.log(document.getElementById("btn-avancar"));
+
+const btnAvancar = document.getElementById("btn-avancar");
+const cartoes = document.querySelectorAll(".cartao");
+const btnVoltar = document.getElementById("btn-voltar")
+let cartaoAtual = 0;
+
+cartoes.forEach(cartao => {
+    cartao.addEventListener("click", function () {
+        const cartaVirada = cartao.querySelector(".carta-virada")
+        cartao.classList.toggle("virar");
+        cartaVirada.classList.toggle("mostrar-fundo-carta");
+        const descricao = cartao.querySelector(".descricao");
+        descricao.classList.toggle("esconder");
+    });
+});
+
+btnAvancar.addEventListener("click", function () {
+    if (cartaoAtual === cartoes.length - 1) return;
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual++;
+    mostrarCartao(cartaoAtual);
+});
+
+btnVoltar.addEventListener("click", function () {
+    if (cartaoAtual === 0) return;
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual--;
+    mostrarCartao(cartaoAtual);
+});
+
+function mostrarCartao() {
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+}
